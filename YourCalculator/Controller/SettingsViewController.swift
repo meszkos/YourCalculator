958ab428125f8fb636e9ed7282a7290006a7c27a
meszkos <michalik515@gmail.com>
2022-02-08 13:43:59 +0100

    Design settings UI, allow changing theme

--- YourCalculator/Controller/SettingsViewController.swift
+++ YourCalculator/Controller/SettingsViewController.swift
@@ -18,6 +18,10 @@ class SettingsViewController: UIViewController {
     var calculatorManager = CalculatorManager()
     var delegate: SettingsDelegate?
     
+    var pickedBackgroundColor: String?
+    var pickedOperationColor: String?
+    var pickedNumberColor: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         backgroundView.backgroundColor = calculatorManager.hexStringToUIColor(hex: "E5E5E5")
@@ -25,9 +29,48 @@ class SettingsViewController: UIViewController {
 
     @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
         
-        delegate?.updateUI(backgroundColor: K.kiwiBackgroundColor, operationColor: K.kiwiOperationColor, numberColor: K.kiwiNumberColor)
+        if pickedBackgroundColor != nil, pickedOperationColor != nil, pickedNumberColor != nil{
+            delegate?.updateUI(backgroundColor: pickedBackgroundColor!, operationColor: pickedOperationColor!, numberColor: pickedNumberColor!)
+        }
         
         
         self.navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func pickThemeButtonPressed(_ sender: UIButton) {
+        
+        //Make it save thame after terminating app
+        
+        if let pickedTheme = sender.restorationIdentifier{
+            switch pickedTheme{
+            case "Sea":
+                pickedBackgroundColor = K.seaBackgroundColor
+                pickedOperationColor = K.seaOperationColor
+                pickedNumberColor = K.seaNumberColor
+            case "Earth":
+                pickedBackgroundColor = K.earthBackgroundColor
+                pickedOperationColor = K.earthOperationColor
+                pickedNumberColor = K.earthNumberColor
+            case "Chilli":
+                pickedBackgroundColor = K.chilliBackgroundColor
+                pickedOperationColor = K.chilliOperationColor
+                pickedNumberColor = K.chilliNumberColor
+            case "Sunset":
+                pickedBackgroundColor = K.sunsetBackgroundColor
+                pickedOperationColor = K.sunsetOperationColor
+                pickedNumberColor = K.sunsetNumberColor
+            case "Space":
+                pickedBackgroundColor = K.spaceBackgroundColor
+                pickedOperationColor = K.spaceOperationColor
+                pickedNumberColor = K.spaceNumberColor
+            case "Kiwi":
+                pickedBackgroundColor = K.kiwiBackgroundColor
+                pickedOperationColor = K.kiwiOperationColor
+                pickedNumberColor = K.kiwiNumberColor
+            default:
+                print("es")
+            }
+            
+        }
     }
 }
